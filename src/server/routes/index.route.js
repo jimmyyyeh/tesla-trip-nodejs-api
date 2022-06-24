f11fa7a100ab5e5786c82f64a0b1a58b130e4ad2
Jimmy Yeh <chienfeng0719@gmail.com>
2022-06-24 17:00:39 +0800

    Comment prouduct and qrcode routes

--- src/server/routes/index.route.js
+++ src/server/routes/index.route.js
@@ -4,8 +4,8 @@ const administrativeDistrict = require('./administrative-district.route');
 const car = require('./car.route');
 const carModel = require('./car-model.route');
 const charger = require('./charger.route');
-const product = require('./product.route');
-const qrcode = require('./qrcode.route');
+// const product = require('./product.route');
+// const qrcode = require('./qrcode.route');
 const trip = require('./trip.route');
 const tripRate = require('./trip-rate.route');
 
@@ -17,8 +17,8 @@ router.use('/administrative-district', administrativeDistrict.router);
 router.use('/car', car.router);
 router.use('/car-model', carModel.router);
 router.use('/super-charger', charger.router);
-router.use('/product', product.router);
-router.use('/qrcode', qrcode.router);
+// router.use('/product', product.router);
+// router.use('/qrcode', qrcode.router);
 router.use('/trip', trip.router);
 router.use('/tripRate', tripRate.router);
 
